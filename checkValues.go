271cb4cc4d4b28d5c3f6b6cc8b8c62dd4f6bae78
Stiffpixels <saifimuzammil2@gmail.com>
2024-12-26 10:10:14 +0530

    added more utils to make creation icon more streamline

--- checkValues.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-func CheckValue(currentValue, defaultValue string) string {
-    if currentValue == ""{
-        return defaultValue
-    }else{
-        return currentValue
-    }
-}
--- utils/utils.go
+++ utils/utils.go
@@ -1,11 +1,12 @@
 package utils
 
+import "github.com/a-h/templ"
+
 func CheckValue(currentValue, defaultValue string) string {
     if currentValue == "" {
-        return currentValue
+        return defaultValue
     }
-
-    return defaultValue
+    return currentValue
 }
 
 type IconProps struct {
@@ -17,4 +18,20 @@ type IconProps struct {
     StrokeLinecap,
     StrokeLinejoin,
     Size string
+}
+
+func getBasicProps(props IconProps) *templ.Attributes{
+    return &templ.Attributes{
+        "xmlns":"http://www.w3.org/2000/svg",
+        "width":CheckValue(props.Size, "24"),
+        "height":CheckValue(props.Size, "24"),
+        "viewBox":CheckValue(props.ViewBox, "0 0 24 24"),
+        "fill":CheckValue(props.Fill, "none"),
+        "stroke":CheckValue(props.Stroke, "currentColor"),
+        "stroke-width":CheckValue(props.StrokeWidth, "2"),
+        "stroke-linecap":CheckValue(props.StrokeLinecap, "round"),
+        "stroke-linejoin":CheckValue(props.StrokeLinejoin, "round"),
+        "class":CheckValue(props.Class, "lucide lucide-chevron-left"),
+        "style":"display:block;",
+    }
 }
